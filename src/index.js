ceaec30d839c38b536241768163749f994202095
sotirelisc <gtakriz@hotmail.com>
2019-05-31 20:07:55 +0300

    Setup basic Redux & persist.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import reducers from './reducers';
+
 import App from './components/App';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware())
+);
+
+const persistor = persistStore(store);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.querySelector('#root')
 );
